7046cf86b9bbcf433fd23b26303aaff918c6ad0d
Kunal Singh <82411321+KunalSin9h@users.noreply.github.com>
2021-10-28 10:27:39 +0530

    update vimrc and template cpp file

--- Template/cpp1.cpp
+++ Template/cpp1.cpp
@@ -1,30 +1,40 @@
-#include "bits/stdc++.h"
+#include <bits/stdc++.h>
 using namespace std;
 using str = string;
+
 typedef long long ll;
+typedef size_t st;
+
+#define F first
+#define S second
+#define pb push_back
 #define nl '\n'
 #define MOD (1000000007)
 #define PI (3.141592653589)
+#define all(x) begin(x), end(x)
 #define deb(x) cout << "[ " << #x << " = " << x << " ]" << '\n'
 #define print(vec) for(auto i: vec){cout << i << " ";} cout << '\n'
-//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-// Globals...
-
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+// Globals..
 
 void solve(){
     // input
-    
+
 
 }
-//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 int main(){
 
     // author: Kunal Singh (KunalSin9h)
 
     ios_base::sync_with_stdio(false), cin.tie(NULL), cout.tie(NULL);
     // Functions....
+    
+
     int Test = 1;
     cin >> Test;
-    while(Test--){solve();}
+    while(Test--){
+        solve();
+    }
     return 0;
 }
