9d33d1ecac73917ae1a07d5794313e020a08105d
Kunal Singh <kunalsin9h@gmail.com>
2021-11-19 21:33:37 -0800

    Update Template/skeleton.cpp

--- Template/skeleton.cpp
+++ Template/skeleton.cpp
@@ -9,12 +9,11 @@ typedef unsigned long long ull;
 #define pss pair<str, str>
 //containers
 #define vi vector<int>
-#define vet(x) vector<x>
-#define unset(x) unordered_set<x>
+#define unset unordered_set
+#define unmap unordered_map
 //loops
 #define F0(n) for(int i=0;i<n;++i)
 #define F1(n) for(int i=1;i<=n;++i)
-#define Fin(x, n) for(int i=x;i<=n;++i) 
 #define FOR(x, y) for(int x=0;x<y;++x)
 #define print(arr, n) F0(n){cout << arr[i] << " ";} cout << nl;
 #define read(arr, n) F0(n){cin >> arr[i];}
@@ -25,7 +24,6 @@ typedef unsigned long long ull;
 #define sz(x) ((int)x.size())
 #define all(x) begin(x), end(x)
 //others
-#define take(a) int a;cin>>a;
 #define cut cout<<nl
 #define nl '\n'
 #define MOD (1000000007)
@@ -36,7 +34,7 @@ typedef unsigned long long ull;
 
 
 void solve(){
-		
+	
 	
 
 
