a0d2e0477e23cb5ee815f86ffc15993572b7c4b8
Kunal Singh <kunalsin9h@gmail.com>
2021-11-25 10:45:49 -0800

    Update Template/.cpp

--- Template/skeleton.cpp
+++ Template/skeleton.cpp
@@ -1,34 +1,27 @@
 #include <bits/stdc++.h>
 using namespace std;
 using str = string;
-//typedefs
 typedef long long ll;
 typedef unsigned long long ull;
-//pair
 #define pii pair<int, int>
 #define pss pair<str, str>
-//containers
-#define vi vector<int>
+#define vt vector
 #define unset unordered_set
 #define unmap unordered_map
-//loops
 #define F0(n) for(int i=0;i<n;++i)
 #define F1(n) for(int i=1;i<=n;++i)
 #define FOR(x, y) for(int x=0;x<y;++x)
 #define print(arr, n) F0(n){cout << arr[i] << " ";} cout << nl;
 #define read(arr, n) F0(n){cin >> arr[i];}
-//methods
-#define F first
-#define S second
+#define f first
+#define s second
 #define pb push_back
 #define sz(x) ((int)x.size())
 #define all(x) begin(x), end(x)
-//others
-#define cut cout<<nl
+#define ter cout<<nl
 #define nl '\n'
 #define MOD (1000000007)
 #define PI (3.141592653589)
-//debug
 #define deb(x) cout << "[ " << #x << " = " << x << " ]" << '\n'
 //-------------------------------------------------------------
 
@@ -44,7 +37,8 @@ int main(){
 
     int Test = 1;
     cin >> Test;
-    for(int tc = 1; tc <= Test; tc++){
+	F1(Test){
+		//cout << "Case #" << i << ": ";
 		solve();
     }
     return 0;
